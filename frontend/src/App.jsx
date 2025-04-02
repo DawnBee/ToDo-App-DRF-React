baedb20404085c228621de4bc0e57259ce9f808b
DawnBee <phalanx882@gmail.com>
2025-04-02 17:50:12 +0800

    Enhanced stylings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import ListTasks from './components/ListTasks';
+import Logo from './assets/imgs/meddicc-logo.png'
 
 function App() {
 
   return (
     <>
+      <div className='logo-box'>
+        <img src={Logo} alt="logo" />
+        <h1>MEDDICC</h1>
+      </div>
       <ListTasks />
     </>
   )
