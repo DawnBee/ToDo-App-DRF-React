4d7540db2073b0cc9262f043e1714672e7839c96
DawnBee <phalanx882@gmail.com>
2025-04-02 14:56:29 +0800

    Modified 'models' and 'serializers'

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,6 +1,30 @@
+from django_resized import ResizedImageField
 from django.db import models
 import uuid
 
+class Attachment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = ResizedImageField(size=[400,400], quality=85, upload_to='attachments/', null=True, blank=True)
+
+    def __str__(self):
+        return f"Attachment for task {self.task.name}"
+
+class Category(models.Model):
+    CATEGORY = [
+        ("Personal", "Personal"),
+        ("Work", "Work"),
+        ("Others", "Others"),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=20, unique=True, choices=CATEGORY, default="Personal")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = 'Categories'
+    
 class Priority(models.Model):
     LEVELS = [
         ("Urgent", "Urgent"),
@@ -13,10 +37,14 @@ class Priority(models.Model):
 
     def __str__(self):
         return self.level
+
+    class Meta:
+        verbose_name = "Priority"
+        verbose_name_plural = 'Priorities'
     
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
     due_date = models.DateTimeField(blank=True, null=True)
@@ -24,6 +52,12 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     # Foreign keys
     priority = models.ForeignKey(Priority, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    attachment = models.ForeignKey(Attachment, on_delete=models.CASCADE, related_name='task', null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = 'Tasks'
--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from .models import Task, Priority
+from .models import Task, Priority, Category, Attachment
 
 class TaskSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(queryset=Category.objects.all(), slug_field='name')
     priority = serializers.SlugRelatedField(queryset=Priority.objects.all(), slug_field='level')
+    attachment_image = serializers.ImageField(source='attachment.image', required=False)
+
+    def create(self, validated_data):
+        attachment_data = validated_data.pop('attachment', None)
+        task = Task.objects.create(**validated_data)
+        
+        if attachment_data and 'image' in attachment_data:
+            attachment = Attachment.objects.create(image=attachment_data['image'])
+            task.attachment = attachment
+        
+        return task
+    
     class Meta:
         model = Task
-        fields = ['id', 'name', 'description', 'completed', 'due_date', 'created_at', 'priority']
+        fields = ['id', 'name', 'description', 'completed', 'due_date', 'created_at', 'priority','category','attachment_image']
