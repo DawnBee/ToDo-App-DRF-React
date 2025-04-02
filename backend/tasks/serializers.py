a1c812a9b1a6245f7606457b6d9050ffc2c2d0f9
DawnBee <phalanx882@gmail.com>
2025-04-02 09:20:51 +0800

    Modified backend files for bonus points

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Priority, Category, Attachment
+class PrioritySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Priority
+        fields = ['id', 'level']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class AttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attachment
+        fields = ['id', 'image', 'uploaded_at']
 
 class TaskSerializer(serializers.ModelSerializer):
+    priority = PrioritySerializer()
+    category = CategorySerializer()
+    attachment = AttachmentSerializer()
+
     class Meta:
         model = Task
-        fields = ['id', 'name', 'description', 'completed', 'due_date']
+        fields = ['id', 'name', 'description', 'completed', 'due_date', 'created_at', 'priority', 'category', 'attachment']
